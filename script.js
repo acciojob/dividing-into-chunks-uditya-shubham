e78538e9987ac42aa58b0e70347a7acfd698f30a
Uditya Kumar <shubhamkumaar2001@gmail.com>
2024-05-24 17:06:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,6 +2,35 @@ const arr = [1, 2, 3, 4, 1, 0, 2, 2];
 
 const divide = (arr, n) => {
   // Write your code here
+	let sum=0;
+	for(let i=0;i<n;i++)
+		{
+			sum+=arr[i];
+			
+		}
+
+	let subsum=sum/s;
+	subsum=parseInt(subsum);
+	
+	let ans=[];
+	for(let i=0; i<n ; i++)
+		{
+			let tempArr=[];
+			let tempsum=0;
+
+			while(tempsum<=subsum)
+				{
+					tempArr.push(arr[i]);
+					tempsum+=arr[i];
+					i++;
+					
+				}
+			i--;
+			ans.push(tempArr);
+			
+		}
+
+	return ans;
 };
 
 const n = prompt("Enter n: ");
